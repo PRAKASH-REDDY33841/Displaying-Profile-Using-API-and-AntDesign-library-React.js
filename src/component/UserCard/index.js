42bd1ae5d4c56c9db964a217ca4e207eb7ee879d
prakash95150 <gnana9515@gmail.com>
2025-09-19 01:28:11 +0530

    Describe what you changed

--- src/component/UserCard/index.js
+++ src/component/UserCard/index.js
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
-import { Component } from "react"; 
-=======
 import { Component } from "react";
-
->>>>>>> 1a3583dcea6b524a208ca481c6bfa4b504211891
 
 import DisplayDetails from "../DisplayDetails";
 
 import "./index.css";
 
 class UserCard extends Component {
-<<<<<<< HEAD
-  state = { profileDetails: []}; 
-=======
-  state = { profileDetails: []}; // ✅ added isLoading state
->>>>>>> 1a3583dcea6b524a208ca481c6bfa4b504211891
+  state = { profileDetails: [] }; 
 
   componentDidMount() {
     this.getProfileData();
@@ -25,7 +16,7 @@ class UserCard extends Component {
       const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
       if (response.ok === true) {
         const data = await response.json();
-        this.setState({ profileDetails: data }); 
+        this.setState({ profileDetails: data}); 
       }
     } catch (error) {
       console.log("getting error in code");
@@ -34,29 +25,16 @@ class UserCard extends Component {
   };
 
   render() {
-<<<<<<< HEAD
-    const { profileDetails } = this.state;
-=======
     const { profileDetails} = this.state;
->>>>>>> 1a3583dcea6b524a208ca481c6bfa4b504211891
 
     return (
       <div className="bg-container">
         <h1 className="heading-ele">Display Profile Details</h1>
 
-<<<<<<< HEAD
-       {profileDetails.map((eachItem) => (
+        {
+          profileDetails.map((eachItem) => (
             <DisplayDetails key={eachItem.id} eachProfileItem={eachItem} />
           ))}
-=======
-        
-    {profileDetails.map((eachItem) => (
-  <DisplayDetails 
-    key={eachItem.id} 
-    eachProfileItem={eachItem} 
-  />
-))}
->>>>>>> 1a3583dcea6b524a208ca481c6bfa4b504211891
       </div>
     );
   }
